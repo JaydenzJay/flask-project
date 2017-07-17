ade7616fe7e65f3e7165d41f415f06b62e364a7c
Joseph Amenyogbe <jaydenzlipton@gmail.com>
2017-07-17 14:19:17 +0000

    Good Work

--- myapp.py
+++ myapp.py
@@ -10,6 +10,18 @@ def index():
 def whereami():
 	return "kdua"
 
+@app.route('/linux')
+def linux():
+	return render_template("linux.html")
+
+@app.route('/python')
+def python():
+	return render_template("python.html")
+
+@app.route('/hello/<name>')
+def foo(name):
+    return render_template('index.html', to=name)
+
 if __name__ == '__main__':
 	app.run(host="0.0.0.0")
 
